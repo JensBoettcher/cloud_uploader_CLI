9a95b8fe8b58f6f5676876dba4f5aa8a85b2bf79
Jens Böttcher <ifrit@mailbox.org>
2023-11-17 14:47:55 +0100

    blob storage creation automated v0.5

--- cloud_uploader.sh
+++ cloud_uploader.sh
@@ -7,7 +7,8 @@ echo "Welcome to the cloud uploader. This script will help you upload files quic
 
 #ANCHOR - variable section
 resource_group="storage_group"
-storage_account="storage20231116"
+storage_account="storage202311200"
+subscription_id="a4382a28-de0e-4825-b036-9b96b837f5e3"
 blob_container="blob20231116"
 
 #ANCHOR - resource-group section
@@ -15,6 +16,7 @@ az group create \
     --name $resource_group \
     --location germanywestcentral
 
+echo "Create your resource-group."
 
 #ANCHOR - storage section
 #NOTE - create storage account
@@ -27,5 +29,21 @@ az storage account create \
     --sku Standard_LRS \
     --encryption-services blob
 
-#NOTE - create the blob storage container
+echo "The storage account creation is in progress."
 
+#NOTE - create the blob storage container
+#NOTE - set storage blob data contributor
+az ad signed-in-user show --query id -o tsv | az role assignment create \
+    --role "Storage Blob Data Contributor" \
+    --assignee @- \
+    --scope "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.Storage/storageAccounts/$storage_account"
+
+echo "Blob storage contributor role added."
+
+#NOTE - create the container
+az storage container create \
+    --account-name $storage_account \
+    --name $blob_container \
+    --auth-mode login
+
+echo "Blob Container created."
